71d6b0e02bfefa1e6fd3b70def00d0d47e929964
Nathan <namyot47@gmail.com>
2026-09-15 11:27:09 -0700

    changed percentage of tail being analyzed for convergence.

--- amber_pipeline/pipeline/convergence.py
+++ amber_pipeline/pipeline/convergence.py
@@ -53,7 +53,7 @@ rms ToRef reference {mask} out rmsd_check.dat
 def is_rmsd_plateaued(prmtop: str, traj: str, ref: str, slope_tol: float = 0.0005,
                        stride: int = 10, work_dir: str = ".") -> bool:
     rmsd = strided_rmsd(prmtop, traj, ref, stride=stride, work_dir=work_dir)
-    tail = rmsd[int(len(rmsd) * 0.5):]
+    tail = rmsd[int(len(rmsd) * 0.7):] #Looks at last 30% of rmsd data to determine convergence
     slope = np.polyfit(range(len(tail)), tail, 1)[0]
     return abs(slope) < slope_tol
 
